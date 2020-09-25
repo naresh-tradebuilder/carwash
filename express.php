46be2450b4e2c78188d79bb898de1767a2cf0544
sahil <sahil@tradebuilderinc.com>
2020-09-25 17:57:49 +0530

    design

--- express.php
+++ express.php
@@ -184,15 +184,15 @@
 							<div class="row mb-1">
 								<div class="col-12">
 									<!-- planFullList -->
-									<article class="planFullList mb-5 mb-sm-8 overflow-hidden rounded">
+									<article class="active planFullList mb-5 mb-sm-8 overflow-hidden rounded">
 										<!-- priceHeader -->
-										<header class="priceHeader px-3 text-capitalize">
+										<header class="bg-primary priceHeader text-white px-3 text-capitalize">
 											<div class="row align-items-center">
 												<div class="col-8">
-													<h2 class="ml-md-3 mb-0 headingIX">QUICK WASH</h2>
+													<h2 class="ml-md-3 mb-0 text-white headingIX">QUICK WASH</h2>
 												</div>
 												<div class="col-4 text-right">
-													<h3 class="headingIX fwSemibold mb-0">$6.99</h3>
+													<h3 class="text-white headingIX fwSemibold mb-0">$6.99</h3>
 												</div>
 											</div>
 										</header>
@@ -209,7 +209,7 @@
 								</div>
 								<div class="col-12">
 									<!-- planFullList -->
-									<article class="planFullList active mb-5 mb-sm-8 shadow-lg overflow-hidden rounded">
+									<article class="planFullList  mb-5 mb-sm-8 overflow-hidden rounded">
 										<!-- priceHeader -->
 										<header class="bg-primary priceHeader text-white px-3 text-capitalize">
 											<div class="row align-items-center">
@@ -239,13 +239,13 @@
 									<!-- planFullList -->
 									<article class="planFullList mb-5 mb-sm-8 overflow-hidden rounded">
 										<!-- priceHeader -->
-										<header class="priceHeader px-3 text-capitalize">
+										<header class="bg-primary priceHeader px-3 text-capitalize">
 											<div class="row align-items-center">
 												<div class="col-8">
-													<h2 class="ml-md-3 mb-0 headingIX">BEST EXTERIOR WASH</h2>
+													<h2 class="ml-md-3 mb-0 headingIX text-white">BEST EXTERIOR WASH</h2>
 												</div>
 												<div class="col-4 text-right">
-													<h3 class="headingIX fwSemibold mb-0">$14.99 </h3>
+													<h3 class="text-white headingIX fwSemibold mb-0">$14.99 </h3>
 												</div>
 											</div>
 										</header>
